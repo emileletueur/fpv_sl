d85513e969f5818d5c8e492c3683795766aadbf8
SGALINA <galina.sebastien@gmail.com>
2026-01-21 22:38:28 +0100

    usb msc

--- src/fpv_sl.c
+++ src/fpv_sl.c
@@ -1,13 +1,43 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 
+#include "bsp/board_api.h"
+#include "tusb.h"
 
-int main()
-{
-    stdio_init_all();
+int main() {
+    
+  board_init();
+  stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+  while (true) {
+    printf("Hello, world!\n");
+    sleep_ms(1000);
+  }
+
+//   bool isDeviceEnumerated = false;
+//   uint32_t start_time = to_ms_since_boot(get_absolute_time());
+
+//   board_init();
+//   tusb_init();
+
+//   while (to_ms_since_boot(get_absolute_time()) - start_time <
+//          USB_ENUM_TIMEOUT_MS) {
+//     tud_task();
+//     if (tud_mounted()) {
+//       isDeviceEnumerated = true;
+//       break;
+//     }
+//   }
+
+//   if (isDeviceEnumerated) {
+//     while (1) {
+//       tud_task();
+//     }
+//   } else {
+//     tud_disconnect();
+//     sleep_ms(100);
+//     boot_micropython();
+//   }
+
+//   return 0;
 }
